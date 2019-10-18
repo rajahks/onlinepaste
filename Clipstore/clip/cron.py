70217a35da23a190160936dec59341c25883b13c
Rajah Yoghindra KS <yoghi.raju@gmail.com>
2019-10-18 23:28:53 +0530

    Replacing print statements with logger calls

--- Clipstore/clip/cron.py
+++ Clipstore/clip/cron.py
@@ -3,27 +3,34 @@ from clip.models import Clip
 from datetime import datetime,timezone
 from clip.models import testFun
 
+import logging
+logger = logging.getLogger(__name__)
+
 class DelExpiredPosts(CronJobBase):
     RUN_EVERY_MINS = 1 # every 5 mins
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'clip.del_expired_posts'    # a unique code
-    print("$$ class ran " )
+    # print("$$ class ran " )
     clipList = Clip.objects.all()
-    print("clipList: " + str(clipList))
+    # print("clipList: " + str(clipList))
+    logger.info("DelExpiredPosts was triggered. count=%d"%(clipList.count(),))
 
     def do(self):
-        print("$$ job ran. Count:"  + str(self.clipList.count()) )
-        print("$$ job ran Entries:" +  str(self.clipList.all()) )
+        # print("$$ job ran. Count:"  + str(self.clipList.count()) )
+        # print("$$ job ran Entries:" +  str(self.clipList.all()) )
         # //testFun()
+        logger.info("DelExpiredPosts do function was triggered. count=%d list: %s"%(self.clipList.count(),str(self.clipList.all()),))
         for clip in self.clipList.all():
-            print("$$ clip:" + str(clip))
+            # print("$$ clip:" + str(clip))
             curTime = datetime.now(tz=timezone.utc)
             if curTime > clip.expiry_date:
-                print("Exipired clip: " + str(clip))
+                # print("Exipired clip: " + str(clip))
                 clipName = str(clip)
                 clip.delete()
-                print("\tDeleted clip:" + clipName)
+                # print("\tDeleted clip:" + clipName)
+                logger.info("Expired clip %s deleted"%(clipName,))
             else:
-                print("Clip: "+ str(clip) +" not yet expired")
+                # print("Clip: "+ str(clip) +" not yet expired")
+                logger.info("Clip: "+ str(clip) +" not yet expired")
         return
--- Clipstore/clip/views.py
+++ Clipstore/clip/views.py
@@ -5,12 +5,23 @@ from django.utils.text import slugify
 import random, string
 import datetime
 
+import logging
+#Get an instance of a logger
+logger = logging.getLogger(__name__)
+# logger = logging.getLogger('django')
+
 # These are keyword which take us to views and so cannot be used while
 # creating a clip.
 blockedKeywords = [ 'admin', 'iskeywordused', 'faq' ]
 
 # Create your views here.
-def check_post(request,clipId):
+
+def check_post(request,clipId):  #TODO: Rename to a more meaningfull name.
+    """
+    This function handles the GET and POST requests. It fetches the object
+    and renders the response
+    """
+
     # get the complete uri pertaining to this clip.
     # clipUri = request.build_absolute_uri(clipId)
     if request.is_secure():
@@ -18,7 +29,7 @@ def check_post(request,clipId):
     else:
         clipUri = 'http://' + request.get_host() + '/' + clipId
 
-    print("fullpath" + clipUri)
+    logger.debug("ClipUri=" + clipUri)
 
     if request.method != 'POST':
         try:
@@ -29,6 +40,7 @@ def check_post(request,clipId):
             # object has been accessed. If Delete on access set. Remove it.
             if clip.link_duration == 0:
                 Clip.objects.filter(clipId=clipId).delete()
+                logger.debug("Deleted Clip %s"%(clipId,))
 
             return render(request,'clip/clip_detail.html',{ 'clipId':clipId,
                                                             'clip':clip }
@@ -38,7 +50,7 @@ def check_post(request,clipId):
             # to create it
             clipForm = ClipForm()
             clipUri = request.build_absolute_uri()
-            print("Clip Doesnot exist BAU:" + clipUri)
+            logger.info("User accessing Clip which Doesnot exist uri:%s. Redirecting to index view" % (clipUri,))
             # context = { 'clipId':clipId, 'uri':clipUri, 'form':clipForm }
 
             # return render(request,'clip/clip_create.html', context )
@@ -50,18 +62,18 @@ def check_post(request,clipId):
         # double POST can cause multiple objects to be created.
         # check if the object exists before creating it.
         if Clip.objects.filter(clipId=clipId).count() !=0:
-            print("DOUBLE POST!!")
+            logger.warning("Double post for clip:%s"%(clipId,))
             return redirect(reverse('index'))
 
         # extract the form and the model and save it.
         clipForm = ClipForm(request.POST)
         if clipForm.is_valid():
-            print("Form is valid")
+            # print("Form is valid")
             # from the form get the object
             clipObj = clipForm.save(commit=False)
             clipObj.clipId = clipId
             linkDuration = clipObj.link_duration
-            print("linkDuration:" + str(linkDuration))
+            logger.debug("Clip form is valid. clipId:%s linkDuration:%d"%(clipId,linkDuration))
             timeDelta = 0
             if clipObj.link_duration == 0:
                 #delete on access link. But we allow saving maximum upto 30 days
@@ -76,7 +88,7 @@ def check_post(request,clipId):
             context = { 'clipId':clipId, 'clipObj':clipObj , 'clipUri':clipUri }
             return render(request,"clip/success.html", context )
         else:
-            print("Form is invalid")
+            logger.debug("Form for clip %s is invalid"%(clipId,)) #TODO: Log errors as well here
             context = {'form':clipForm ,'randClipId':clipId}
             return render(request,'clip/index.html',context)
 
@@ -92,30 +104,35 @@ def index(request):
         clipIdToUse = request.POST.get("clip-url")
         # clean it up using slugify
         sClipIdToUse = slugify(clipIdToUse)
-        print("clipId: " + clipIdToUse + " sluggedId:" + sClipIdToUse )
+        logger.debug("clipId: " + clipIdToUse + " sluggedId:" + sClipIdToUse )
 
         try:
             if len(sClipIdToUse) == 0:
-                print("Zero length clip id!" )
                 clipIdErr = "Enter a valid keyword! Consisting of letters, numbers, underscores or hyphens"
             elif sClipIdToUse.lower() in blockedKeywords:
+                logger.debug("Clip id in blocked keywords:" + sClipIdToUse )
                 clipIdErr = "Keyword already in use. Why don't you choose another one?"
             else:
                 clip = Clip.objects.get(clipId=sClipIdToUse)
                 # clip found so id already used.
                 # A non used id will raise a DoesNotExist exception and
                 # we will create a clip using the check_post api
-                print("Clip id already used:" + sClipIdToUse )
+                logger.debug("Clip id already used:" + sClipIdToUse )
                 clipIdErr = "Keyword already in use. Why don't you choose another one?"
 
             clipForm = ClipForm(request.POST)
             context = {'form':clipForm ,'randClipId':sClipIdToUse, 'clipIdErr':clipIdErr}
             return render(request,'clip/index.html',context)
+
         except Clip.DoesNotExist:
             # unused clip id.
             return check_post(request,sClipIdToUse)
 
 def isKeywordUsed(request):
+    """
+    Function used to determine if the keyword is already used.
+    """
+
     used = True
     if request.method == 'GET':
         keyword = request.GET['keyword']
@@ -136,8 +153,19 @@ def isKeywordUsed(request):
 
 
 def getRandomClipId():
+    """
+    Function to generate random clip id. 
+    Returns the random id generated.
+    """
+
     found = False
     size = 5
+    # Number of iterations that were required to find an free keyword.
+    # As long as this number is small we should be OK. However if we
+    # this number is large then we need to refactor this code to have a
+    # efficient method.
+    loopCount = 1 
+    
     while found==False:
         # IMP: removed upper case letters as slugify automatically converts
         # everything to lowercase. This decreases the probability of finding
@@ -147,9 +175,11 @@ def getRandomClipId():
 
         if Clip.objects.filter(clipId=genId).count() == 0:
             # unused keyword found.
+            logger.info("Loop count to find a free keyword:%d keyword:%s"%(loopCount,genId))
             return genId
         else:
             # continue until found. Possibilty of not finding is very low.
+            loopCount += 1
             continue
 
 def faqView(request):
