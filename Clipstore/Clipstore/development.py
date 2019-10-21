3228861bfa83185b74e6b7438fa9cc27507ab161
Rajah Yoghindra KS <yoghi.raju@gmail.com>
2019-10-21 16:04:32 +0530

    Adding Development and Production settings files.

---
+++ Clipstore/Clipstore/development.py
@@ -0,0 +1,9 @@
+from Clipstore.settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'uky2npl7-d#673)!pf)ybsuqw&5kk)ct_wrjdc-w!aivh$6(^d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
--- Clipstore/Clipstore/wsgi.py
+++ Clipstore/Clipstore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Clipstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Clipstore.development')
 
 application = get_wsgi_application()
